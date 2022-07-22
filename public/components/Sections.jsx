5d63224396de604cc78d4def8905f506fe8fc80c
George Sandoval <george.sandoval.estela@gmail.com>
2022-07-22 09:21:25 -0500

    feat add sidebarleft

--- public/components/Sections.jsx
+++ public/components/Sections.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SideBarLeft from "./SideBarLeft";
 
 function Sections() {
-  return <main className="main"></main>;
+  return (
+    <main className="main">
+      <SideBarLeft />
+    </main>
+  );
 }
 
 export default Sections;
--- public/components/SideBarLeft.jsx
+++ public/components/SideBarLeft.jsx
@@ -1,5 +1,56 @@
 import React from "react";
+import Profile from "./Profile";
 
 function SideBarLeft() {
-  return <div></div>;
+  return (
+    <div className="sidebar-left">
+      <Profile />
+      <div className="teams">
+        <h2>My team</h2>
+        <div className="container-team">
+          <div className="date-teams1">
+            <a className="link-team" href="#">
+              <img
+                className="team-img"
+                src="/src/assets/team/team-1.jpg"
+                alt="Alden Cantrell"
+              />
+              <p className="text-teams">Alden Cantrell</p>
+            </a>
+          </div>
+          <div className="date-teams">
+            <a className="link-team" href="#">
+              <img
+                className="team-img"
+                src="/src/assets/team/team-2.jpg"
+                alt="Thomas Crane"
+              />
+              <p>Thomas Crane</p>
+            </a>
+          </div>
+          <div className="date-teams">
+            <a className="link-team" href="#">
+              <img
+                className="team-img"
+                src="/src/assets/team/team-3.jpg"
+                alt="Miranda Shaffer"
+              />
+              <p>Miranda Shaffer</p>
+            </a>
+          </div>
+          <div className="date-teams2">
+            <a className="link-team" href="#">
+              <img
+                className="team-img"
+                src="/src/assets/team/team-4.jpg"
+                alt="Alvaro Mcgee"
+              />
+              <p>Alvaro Mcgee</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+export default SideBarLeft;
