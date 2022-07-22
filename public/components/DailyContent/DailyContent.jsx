b5f374b02c67f943ddd5eb20e5974b8429a65e39
George Sandoval <george.sandoval.estela@gmail.com>
2022-07-22 09:50:03 -0500

    feat implment DailyContent to React

--- public/components/DailyContent/DailyContent.jsx
+++ public/components/DailyContent/DailyContent.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import InputPost from "./InputPost";
+import Post from "./Post";
 
 function DailyContent() {
-  return <div></div>;
+  return (
+    <div className="daily-content">
+      <InputPost />
+      <div className="posts-container">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
 
 export default DailyContent;
--- public/components/Sections.jsx
+++ public/components/Sections.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import DailyContent from "./DailyContent/DailyContent";
 import SideBarLeft from "./SideBarLeft/SideBarLeft";
 
 function Sections() {
   return (
     <main className="main">
       <SideBarLeft />
+      <DailyContent />
     </main>
   );
 }
