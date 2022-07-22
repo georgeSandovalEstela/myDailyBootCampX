06dc9a8ca8c05e625642b8c55952ac2b6abbe88a
George Sandoval <george.sandoval.estela@gmail.com>
2022-07-22 09:29:24 -0500

    feat: sidebarleft to react components

--- public/components/SideBarLeft/SideBarLeft.jsx
+++ public/components/SideBarLeft/SideBarLeft.jsx
@@ -1,55 +1,12 @@
 import React from "react";
 import Profile from "./Profile";
+import Team from "./Team";
 
 function SideBarLeft() {
   return (
     <div className="sidebar-left">
       <Profile />
-      <div className="teams">
-        <h2>My team</h2>
-        <div className="container-team">
-          <div className="date-teams1">
-            <a className="link-team" href="#">
-              <img
-                className="team-img"
-                src="/src/assets/team/team-1.jpg"
-                alt="Alden Cantrell"
-              />
-              <p className="text-teams">Alden Cantrell</p>
-            </a>
-          </div>
-          <div className="date-teams">
-            <a className="link-team" href="#">
-              <img
-                className="team-img"
-                src="/src/assets/team/team-2.jpg"
-                alt="Thomas Crane"
-              />
-              <p>Thomas Crane</p>
-            </a>
-          </div>
-          <div className="date-teams">
-            <a className="link-team" href="#">
-              <img
-                className="team-img"
-                src="/src/assets/team/team-3.jpg"
-                alt="Miranda Shaffer"
-              />
-              <p>Miranda Shaffer</p>
-            </a>
-          </div>
-          <div className="date-teams2">
-            <a className="link-team" href="#">
-              <img
-                className="team-img"
-                src="/src/assets/team/team-4.jpg"
-                alt="Alvaro Mcgee"
-              />
-              <p>Alvaro Mcgee</p>
-            </a>
-          </div>
-        </div>
-      </div>
+      <Team />
     </div>
   );
 }
